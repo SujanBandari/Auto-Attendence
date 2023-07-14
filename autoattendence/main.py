0df2fa3db5366a2270b1ea94537e92b89b531a1b
SujanBandari <93337890+SujanBandari@users.noreply.github.com>
2023-07-14 18:01:03 +0530

    Update main.py

--- autoattendence/main.py
+++ autoattendence/main.py
@@ -4,7 +4,7 @@ import face_recognition
 import os
 from datetime import datetime
 
-# from PIL import ImageGrab
+
 
 path = 'Training_images'
 images = []
@@ -43,7 +43,7 @@ def markAttendance(name):
                 dtString = now.strftime('%H:%M:%S')
                 f.writelines(f'\n{name},{dtString}')
 
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
+# FOR CAPTURING SCREEN RATHER THAN WEBCAM!!
 # def captureScreen(bbox=(300,300,690+300,530+300)):
 #     capScr = np.array(ImageGrab.grab(bbox))
 #     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
@@ -56,7 +56,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-# img = captureScreen()
+    # img = captureScreen()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
@@ -66,12 +66,12 @@ while True:
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-# print(faceDis)
+        # print(faceDis)
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-# print(name)
+            # print(name)
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
